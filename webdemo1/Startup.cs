4ffd7ef29a4ada021cb19517a21ecea77bb57d84
siddu <siddupatil2000@gmail.com>
2018-07-13 11:30:18 +0530

    initial

---
+++ webdemo1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webdemo1.Startup))]
+namespace webdemo1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
